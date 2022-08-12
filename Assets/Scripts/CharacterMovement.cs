566471681154bbb611d02c3b61897c2c93d64dbc
Kwan <jky6216@naver.com>
2022-08-12 09:42:13 +0900

    Implement character movement(No Anmaiton)

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Camera camera;
+    private Animator animator;
+
+    private bool isMove;
+    private Vector3 destination;
+    private float speed;
+    private float minDistance;
+
+    void Awake()
+    {
+        camera = Camera.main;
+        animator = GetComponent<Animator>();
+
+        speed = 5f;
+        minDistance = 0.1f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            SetDestination(hit.point);
+        }
+
+        Move();
+    }
+
+    private void SetDestination(Vector3 dest)
+    {
+        destination = dest;
+        isMove = true;
+        animator.SetBool("isMove", true);
+    }
+
+    private void Move()
+    {
+        if (isMove)
+        {
+            Debug.Log("SetBool(isMove)");
+            var dir = destination - transform.position;
+            transform.position += dir.normalized * Time.deltaTime * speed;
+        }
+
+        if(Vector3.Distance(transform.position, destination) <= minDistance)
+        {
+            isMove = false;
+            animator.SetBool("isMove", false);
+        }
+    }
+}
